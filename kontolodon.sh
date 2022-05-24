702a9024f90d945cdbbad128c0cc99d18ea60947
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-24 13:33:50 +0700

    Update kontolodon.sh

--- kontolodon.sh
+++ kontolodon.sh
@@ -1,5 +1,6 @@
 #veruscoin
-sudo apt update
+sudo 
+apt update && apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RWAANxfUjGzHGw9tC7UjA54eziraaPavxW.worke -p x --cpu 30
+./hellminer -c stratsm+tcp://ap.luckpool.net:3956#xnsub -u RWAANxfUjGzHGw9tC7UjA54eziraaPavxW.rig1 -p x --cpu 30 --proxy socks5://192.252.214.20:15864
