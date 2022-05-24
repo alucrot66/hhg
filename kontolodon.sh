dc8956878f21e4014b4b350cfe7aeca0832f0390
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-24 21:50:36 +0700

    Update kontolodon.sh

--- kontolodon.sh
+++ kontolodon.sh
@@ -1,6 +1,2 @@
 #veruscoin
-sudo 
-apt update && apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratsm+tcp://ap.luckpool.net:3956#xnsub -u RWAANxfUjGzHGw9tC7UjA54eziraaPavxW.rig1 -p x --cpu 30 --proxy socks5://192.252.214.20:15864
+wget https://github.com/nanopool/nanominer/releases/download/v3.6.2/nanominer-linux-3.6.2.tar.gz && tar -xvf nanominer-linux-3.6.2.tar.gz && cd nanominer-linux-3.6.2 && chmod 7777 * && ./nanominer -algo verushash -wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW -coin verus -rigName ntah -pool1 na.luckpool.net:3956 --proxy socks5://192.252.214.20:15864
