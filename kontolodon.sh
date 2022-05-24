f14b038dad5bd2376960a894243a9b9140002b3d
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-24 22:44:06 +0700

    Update kontolodon.sh

--- kontolodon.sh
+++ kontolodon.sh
@@ -1,4 +1,6 @@
-#veruscoin
-#veruscoin
-wget https://github.com/d2n1982/hellminerlama/raw/main/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && cd hellminer && chmod 7777* && ./hellminer -algo verushash -wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW -rigName tes1 -pool1 ap.luckypool.net:3956 --proxy socks5://192.252.214.20:15864
-      
+#verus
+apt update && apt upgrade
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev
+automake autotools-dev build-essential
+apt-get install git
+git clone –single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer  -algo verus  -wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW -rigName tes1 -pool1 ap.luckypool.net:3956 --proxy socks5://192.252.214.20:15864
