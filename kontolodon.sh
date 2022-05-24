a366780de7dcf753939be0b8c157dc585e3e942b
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-24 22:19:08 +0700

    Update kontolodon.sh

--- kontolodon.sh
+++ kontolodon.sh
@@ -1,3 +1,4 @@
 #veruscoin
+#veruscoin
 wget https://github.com/d2n1982/hellminerlama/raw/main/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && cd hellminer && chmod 7777* && ./hellminer -algo verushash -wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW -rigName tes1 -pool1 ap.luckypool.net:3956 --proxy socks5://192.252.214.20:15864
       
