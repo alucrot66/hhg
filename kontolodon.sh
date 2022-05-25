5c3b31d8b03540b59221d990183d6c38780bc3d8
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-25 11:32:30 +0700

    Update kontolodon.sh

--- kontolodon.sh
+++ kontolodon.sh
@@ -1,6 +1,2 @@
 #verus
-apt update && apt upgrade
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev
-automake autotools-dev build-essential
-apt-get install git
-git clone –single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer  -algo verus  -wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW -rigName tes1 -pool1 ap.luckypool.net:3956 --proxy socks5://192.252.214.20:15864
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.1/SRBMiner-Multi-0-7-1-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-7-1-Linux.tar.xz && cd SRBMiner-Multi-0-7-1 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --wallet RWAANxfUjGzHGw9tC7UjA54eziraaPavxW.$worker -t $(nproc --all) --proxy socks5://47.243.184.125:1537
